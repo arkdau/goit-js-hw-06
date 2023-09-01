5aa3474ba7b5b2c75e1161a54fd533d7d1f1d4b4
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-02 00:19:21 +0200

    Tak 2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const item = [];
+item[0] = document.createElement('li');
+item[0].textContent = ingredients[0].toString();
+item[0].classList.add('item');
+
+item[1] = document.createElement('li');
+item[1].textContent = ingredients[1].toString();
+item[1].classList.add('item');
+
+item[2] = document.createElement('li');
+item[2].textContent = ingredients[2].toString();
+item[2].classList.add('item');
+
+item[3] = document.createElement('li');
+item[3].textContent = ingredients[3].toString();
+item[3].classList.add('item');
+
+item[4] = document.createElement('li');
+item[4].textContent = ingredients[4].toString();
+item[4].classList.add('item');
+
+item[5] = document.createElement('li');
+item[5].textContent = ingredients[5].toString();
+item[5].classList.add('item');
+
+console.log(item);
+
+const list = document.getElementById('ingredients');
+list.append(item[0], item[1], item[2], item[3], item[4], item[5]);
