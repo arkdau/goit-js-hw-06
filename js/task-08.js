c0447af3b4ee83ee08aec39fd6b9b43a7d6836e5
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-02 18:39:54 +0200

    Task 8 - done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector('.login-form');
 
+// handler - submit
+function handlerSubmit(event) {
+  event.preventDefault(); // Stop the default action
+  const form = event.target;
+  const {
+    elements: { email, password },
+  } = form;
+  if (email.value === '' && password.value === '') {
+    alert('Email, Password - empty string');
+  } else if (email.value === '') {
+    alert('Email - empty string');
+  } else if (password.value === '') {
+    alert('Password - empty string');
+  } else {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    form.reset();
+  }
+  // console.log(form);
+}
+
+loginForm.addEventListener('submit', handlerSubmit);
