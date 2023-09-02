4e5941eaf81a414d827f587766655b1d749d0485
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-02 21:13:39 +0200

    Task 9 - done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+btn.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = document.body.style.backgroundColor;
+});
