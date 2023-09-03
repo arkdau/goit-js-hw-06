85010c908764e03765dde247ec61a99db3742ca9
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-03 22:56:34 +0200

    fix: added for() iterations

--- js/task-02.js
+++ js/task-02.js
@@ -6,32 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
 const item = [];
-item[0] = document.createElement('li');
-item[0].textContent = ingredients[0].toString();
-item[0].classList.add('item');
+const list = document.getElementById('ingredients');
 
-item[1] = document.createElement('li');
-item[1].textContent = ingredients[1].toString();
-item[1].classList.add('item');
-
-item[2] = document.createElement('li');
-item[2].textContent = ingredients[2].toString();
-item[2].classList.add('item');
-
-item[3] = document.createElement('li');
-item[3].textContent = ingredients[3].toString();
-item[3].classList.add('item');
-
-item[4] = document.createElement('li');
-item[4].textContent = ingredients[4].toString();
-item[4].classList.add('item');
-
-item[5] = document.createElement('li');
-item[5].textContent = ingredients[5].toString();
-item[5].classList.add('item');
+for (let i = 0; i < 6; i += 1) {
+  item[i] = document.createElement('li');
+  item[i].textContent = ingredients[i].toString();
+  item[i].classList.add('item');
+  list.append(item[i]);
+}
 
 console.log(item);
-
-const list = document.getElementById('ingredients');
-list.append(item[0], item[1], item[2], item[3], item[4], item[5]);
